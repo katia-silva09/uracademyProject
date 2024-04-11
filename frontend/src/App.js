2bdd239b8aecba371300d0de7b49ee52a49a83c0
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2024-04-10 21:03:38 -0600

    created card courses

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,197 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 
 function App() {
   return (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a className="navbar-brand" href="#">Uracademy</a>
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Courses</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Blogs</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Documentation</a>
-        </li>
-        {/* <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li> */}
-      </ul>
-      {/* <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-    </div>
-  </div>
-</nav>
-  );
+    <>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+          <div className="container">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <a className="navbar-brand"  href="#">URACADEMY</a>
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Courses</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Blogs</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Documentation</a>
+                </li>
+
+                {/* <li className="nav-item">
+                  <a className="nav-link disabled">Disabled</a>
+                </li> */}
+              </ul>
+              {/* <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form> */}
+            </div>
+          </div>
+      </nav>
+
+<main className='mt-4'>
+        <div className="container">
+        <h3 className='mb-4'>Ultimos Courses<a href="#" className="float-end btn btn-dark"> View Courses <i className="fa-solid fa-arrow-right-long"></i></a></h3> 
+        <div className="row">
+          {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Php</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-2'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Laravel</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-2'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Python</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'><i class="fa-solid fa-cloud-arrow-down"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Django</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Node.js</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">Html</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">React</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-plus"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              {/* Product Box */}
+            <div className="col-12 col-md-3 mb-2">    
+            <div className="card">
+              <img src={logo} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-title">JavaScrip</h4>
+                <h5 className="card-title text-muted">Precio: C$500.00</h5>
+                <div className="card-footer">
+              </div>  
+                  <button title="Agregar al carrito" className= 'btn btn-success btn-sm'> <i className="fa-solid fa-cart-shopping"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-danger btn-sm ms-1'> <i className="fa fa-heart"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-warning btn-sm ms-2'><i class="fa-solid fa-thumbs-down"></i></button>
+                  <button title="Agregar a la lista" className= 'btn btn-primary btn-sm ms-2'><i class="fa-solid fa-bookmark"></i></button>
+              </div>
+            </div>
+            </div>
+              {/* Product Box end */}
+
+              
+          </div>
+          </div>
+</main>
+    </>
+
+
+
+
+);
 }
 
 export default App;
