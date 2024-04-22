2dfa6aef237171169aa321acbe840e91ea477a0b
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2024-04-21 21:00:05 -0600

    creacion de los componentes, creacion de archivo home, footer, header y categories

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,29 @@
+function Footer(){
+    return(
+                  <footer className="d-flex flex-wrap justify-content-between container align-items-center my-4 border-top">
+                  <div className="col-md-4 d-flex align-items-center">
+                  <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                      URACADEMY
+                      </a>
+                          <span className="mb-3 mb-md-0 text-muted">© 2024 Company</span>
+                      </div>
+      
+                      <ul className="nav col-md-4 justify-content-end list-unstyled d-flex pt-2">
+                          <li className="ms-3">
+                              <a className="text-muted" href="www.linkedin.com" target='blank'><i className='fa-brands fa-linkedin fa-2x'></i></a>            
+                          </li>
+                          <li className="ms-3">
+                              <a className="text-muted" href="www.gmail.com" target="_blank"><i className="fab fa-google fa-2x"></i></a>            
+                          </li>
+                          <li className="ms-3">
+                              <a className="text-muted" href="www.twitter.com" target='blank'><i className='fa-brands fa-twitter fa-2x'></i></a>            
+                          </li>
+                          <li className="ms-3">
+                              <a className="text-muted" href="www.github.com" target='blank'><i className='fa-brands fa-github fa-2x'></i></a>            
+                          </li>
+                      </ul>
+                </footer>
+    );
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+function Header(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand"  href="#">URACADEMY</a>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Category</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Blogs</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Documentation</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav> 
+
+    );
+}
+
+export default Header
