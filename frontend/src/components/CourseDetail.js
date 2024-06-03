6123fb94e8ba76d44ee494954a0794599c835728
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2024-06-02 20:06:45 -0600

    update course detail

--- frontend/src/components/CourseDetail.js
+++ frontend/src/components/CourseDetail.js
@@ -1,113 +1,227 @@
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { UserContext, CartContext } from "../Context";
+import RelatedSingleCourses from "./RelatedSingleCourses";
+import axios from "axios";
 
 function CourseDetail() {
   const baseUrl = "http://127.0.0.1:8000/api";
   const [CourseData, setCourseData] = useState([]);
   const [CourseImgs, setCourseImgs] = useState([]);
+  const [relatedCourses, setRelatedCourses] = useState([]);
+  const [cartbuttonClickStatus, setcartButtonClickStatus] = useState(false);
+
+  // Use the CartContext
+  const { cartData, setCartData } = useContext(CartContext);
+
   const { course_id } = useParams();
 
   useEffect(() => {
     fetchData(baseUrl + "/course/" + course_id);
+    fetchRelatedData(baseUrl + "/related-courses/" + course_id);
+    checkCourseInCart(course_id);
   }, [course_id]);
 
+  function checkCourseInCart(course_id) {
+    var previousCart = localStorage.getItem("cartData");
+    var cartJson = previousCart ? JSON.parse(previousCart) : [];
+
+    if (cartJson && Array.isArray(cartJson)) {
+      cartJson.forEach((cart) => {
+        if (cart && cart.course && cart.course.id === parseInt(course_id)) {
+          setcartButtonClickStatus(true);
+        }
+      });
+    }
+  }
+
   function fetchData(baseurl) {
-    fetch(baseurl)
-      .then((response) => response.json())
-      .then((data) => {
+    axios.get(baseurl)
+      .then((response) => {
+        const data = response.data;
         console.log(data);
         setCourseData(data);
         setCourseImgs(data.course_imgs || []);
-      });
+      })
+      .catch((error) => console.error("Error fetching course data", error));
   }
 
-  return (
-    <section style={{fontFamily: 'ADLaM Display', backgroundColor: 'DarkSlateGray', marginTop: 0, paddingTop: 20, paddingBottom: 90 , color:'white', fontSize:30, textDecoration: 'none' }}>
-    <div className="container">
-      <br/>
-      <h3 className="mb-4" style={{fontSize:40}}>Detalle del curso</h3>
-      <div className="row">
-        <div className="col-4">
-          <div id="relatedThumbnailsSlider" className="carousel carousel-dark slide carousel-fade" data-bs-ride="true">
-            
-            <div className="carousel-inner">
-              {
-                CourseImgs.map((img, index) => {
-                  if (index === 0) {
-                    return (
-                      <div className="carousel-item active" key={index}>
-                        <img src={img.image} className="img-thumbnail mb-5" alt={index} />
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div className="carousel-item" key={index}>
-                        <img src={img.image} className="img-thumbnail mb-5" alt={index} />
-                      </div>
-                    );
-                  }
-                })
-              }
-            </div>
-          </div>
-          
-        </div>
-        <div className="col-8">
-          <h4 className="text-info" style={{fontSize:40, textDecoration: 'none' }}>{CourseData.title}</h4>
-          <p style={{fontSize:25}}>{CourseData.details}</p>
-          <p className="text-warning" style={{fontSize:20}}>Precio: ${CourseData.price}</p>
-          <button title="Demo" target="_blanck" className="btn btn-dark">
-            <i className="fa-solid fa-cart-plus"></i> Demo
-          </button>
-          <button title="Agregar al carrito" target="_blanck" className="btn btn-primary ms-1">
-            <i className="fa-solid fa-cart-plus"></i> Agregar al carrito
-          </button>
-          <button title="Comprar ahora" target="_blanck" className="btn btn-success ms-1">
-            <i className="fa-solid fa-credit-card"></i> Comprar ahora
-          </button>
-          <button title="Agregar a la lista" target="_blanck" className="btn btn-danger ms-1">
-            <i className="fa-solid fa-tag"></i> Agregar a la lista
-          </button>
-        </div>
-      </div>
+  function fetchRelatedData(url) {
+    axios.get(url)
+      .then((response) => {
+        setRelatedCourses(response.data.results);
+      })
+      .catch((error) => console.error("Error fetching related courses", error));
+  }
 
-      {/* Related carousel products */}
-      <h3 className="mt-5 mb-3 text-center">Productos Relacionados</h3>
-      <div id="relatedProductSlider" className="carousel-dark slide" data-bs-ride="true">
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  const cartAddButtonHandler = () => {
+    var previousCart = localStorage.getItem("cartData");
+    var cartJson = previousCart ? JSON.parse(previousCart) : [];
+    var newCartItem = {
+      course: {
+        id: CourseData.id,
+        title: CourseData.title,
+        price: CourseData.price,
+        image: CourseData.image,
+      },
+      user: {
+        id: 1,
+      },
+    };
+
+    cartJson.push(newCartItem);
+    localStorage.setItem("cartData", JSON.stringify(cartJson));
+    setCartData(cartJson);
+    console.log("agregado al carrito");
+    setcartButtonClickStatus(true);
+  };
+
+  const cartRemoveButtonHandler = () => {
+    var previousCart = localStorage.getItem("cartData");
+    var cartJson = previousCart ? JSON.parse(previousCart) : [];
+    
+    cartJson = cartJson.filter(cart => cart.course.id !== parseInt(course_id));
+    localStorage.setItem("cartData", JSON.stringify(cartJson));
+    setCartData(cartJson);
+    console.log("Eliminado del carrito");
+    setcartButtonClickStatus(false);
+  };
+
+  return (
+    <section
+      style={{
+        fontFamily: "ADLaM Display",
+        backgroundColor: "DarkSlateGray",
+        marginTop: 0,
+        paddingTop: 20,
+        paddingBottom: 90,
+        color: "white",
+        fontSize: 30,
+        textDecoration: "none",
+      }}
+    >
+      <div className="container">
+        <br />
+        <h3 className="mb-4" style={{ fontSize: 40 }}>
+          Detalle del curso
+        </h3>
+        <div className="row">
+          <div className="col-4">
+            <div
+              id="relatedThumbnailsSlider"
+              className="carousel carousel-dark slide carousel-fade"
+              data-bs-ride="true"
+            >
+              <div className="carousel-inner">
+                {CourseImgs.map((img, index) => {
+                  return (
+                    <div
+                      className={`carousel-item ${index === 0 ? 'active' : ''}`}
+                      key={index}
+                    >
+                      <img
+                        src={img.image}
+                        className="img-thumbnail mb-5"
+                        alt={index}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-          <div className="carousel-inner">
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
+          <div className="col-8">
+            <h4
+              className="text-info"
+              style={{ fontSize: 40, textDecoration: "none" }}
+            >
+              {CourseData.title}
+            </h4>
+            <p style={{ fontSize: 25 }}>{CourseData.details}</p>
+            <p className="text-warning" style={{ fontSize: 20 }}>
+              Precio: ${CourseData.price}
+            </p>
+            <Link
+              to={CourseData.demo_url}
+              title="Demo"
+              target="_blank"
+              className="btn btn-dark"
+            >
+              <i className="fa-solid fa-cart-plus"></i> Demo
+            </Link>
+            {!cartbuttonClickStatus && (
+              <button
+                title="Agregar al carrito"
+                type="button"
+                onClick={cartAddButtonHandler}
+                className="btn btn-primary ms-1"
+              >
+                <i className="fa-solid fa-cart-plus"></i> Agregar al carrito
+              </button>
+            )}
+            {cartbuttonClickStatus && (
+              <button
+                title="Eliminar del carrito"
+                type="button"
+                onClick={cartRemoveButtonHandler}
+                className="btn btn-danger ms-1"
+              >
+                <i className="fa-solid fa-cart-plus"></i> Eliminar del carrito
+              </button>
+            )}
+            <button
+              title="Comprar ahora"
+              target="_blank"
+              className="btn btn-success ms-1"
+            >
+              <i className="fa-solid fa-credit-card"></i> Comprar ahora
             </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
+            <button
+              title="Agregar a la lista"
+              target="_blank"
+              className="btn btn-danger ms-1"
+            >
+              <i className="fa-solid fa-tag"></i> Agregar a la lista
             </button>
-            <div className="carousel-item active">
-              <div className="row mb-5">
-                {/* Contenido de los productos relacionados */}
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="row mb-5">
-                {/* Contenido de los productos relacionados */}
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="row mb-5">
-                {/* Contenido de los productos relacionados */}
-              </div>
-            </div>
           </div>
         </div>
+
+        {/* Related carousel */}
+        {relatedCourses.length > 0 && (
+          <>
+            <h3 className="mt-5 mb-3 text-center">Cursos Relacionados</h3>
+            <div
+              id="relatedCourseSlider"
+              className="carousel slide carousel-dark"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-indicators">
+                {relatedCourses.map((course, index) => (
+                  <button
+                    type="button"
+                    data-bs-target="#relatedCourseSlider"
+                    data-bs-slide-to={index}
+                    className={index === 0 ? "active" : ""}
+                    aria-current={index === 0 ? "true" : ""}
+                    aria-label={`Slide ${index + 1}`}
+                    key={index}
+                  ></button>
+                ))}
+              </div>
+              <div className="carousel-inner">
+                {relatedCourses.map((course, index) => (
+                  <div
+                    className={`carousel-item ${index === 0 ? 'active' : ''}`}
+                    key={index}
+                  >
+                    <RelatedSingleCourses course={course} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
       </div>
-    </div>
     </section>
   );
 }
